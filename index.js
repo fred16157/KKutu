94c97ec1e37fdcdc1dd10f000a7060cae1da0de9
강현구 <fred16157@gmail.com>
2019-07-24 13:43:42 +0900

    오른쪽에 플레이어 목록 추가

--- index.js
+++ index.js
@@ -1,50 +1,56 @@
+/* eslint-disable no-console */
 //서버에서 동작하는 코드
 //클라이언트에서 동작하는 코드는 index.html의 script 태그로
-
+var blacklist = [];
 var ex = require('express')();
 var http = require('http').createServer(ex);
 var io = require('socket.io')(http);
-const opn = require('opn');
+//const opn = require('opn');
 var userlist = [];
 var currentChar = '';
 var timeout;
 var idx;
 
 ex.set('trust proxy', true);
-var ip;
 ex.get('/', function (req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
     console.log('유저 ' + socket.id + ' 연결됨');
-    io.emit('msg', "유저 " + socket.id + '가 연결되었습니다.');
+    io.emit('msg', "유저 " + socket.id + '가 연결되었습니다.', '#000000');
     socket.username = socket.id;
+    socket.color = '#' + (function co(lor) {
+        return (lor +=
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'][Math.floor(Math.random() * 16)])
+            && (lor.length == 6) ? lor : co(lor);
+    })('');
+    console.log(socket.username + '에 색 코드 ' + socket.color + '가 부여됨');
     userlist.push(socket);
-    socket.on('update', function(username) {
+    updateUsersList();
+    socket.on('update', function (username) {
         console.log("유저 " + socket.username + "의 닉네임이 " + username + "로 변경됨");
-        io.emit('msg', "유저 " + socket.username + "의 닉네임이 " + username + "로 변경되었습니다.");
+        io.emit('msg', "유저 " + socket.username + "의 닉네임이 " + username + "로 변경되었습니다.", '#000000');
         userlist[userlist.indexOf(socket)].username = username;
+        updateUsersList();
     });
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function () {
         console.log(socket.username + ' 의 연결 끊김');
-        io.emit('msg', "유저 " + socket.username + '의 연결이 끊겼습니다.');
+        io.emit('msg', "유저 " + socket.username + '의 연결이 끊겼습니다.', '#000000');
         userlist.splice(userlist.indexOf(socket));
+        updateUsersList();
     });
-    socket.on('msg', function(msg) {
-        io.emit('msg', socket.username + ': ' +msg);
+    socket.on('msg', function (msg) {
+        io.emit('msg', socket.username + ': ' + msg, socket.color);
         console.log(socket.username + ': ' + msg);
-        if(currentChar === '' && msg.split(' ')[0] === '시작')
-        {
+        if (currentChar === '' && msg.split(' ')[0] === '/시작') {
+            currentChar = msg.split(' ')[1].charAt(msg.split(' ')[1].length - 1);
             startGame();
-            currentChar = msg.split(' ')[1];
         }
-        else if(msg.split(' ')[0].charAt(0) === currentChar && currentChar !== '' && userlist[idx].id === socket.id)
-        {
-            raiseAnswered(socket, msg.split(' ')[0]);   
+        else if (msg.split(' ')[0].charAt(0) === currentChar && currentChar !== '' && userlist[idx].id === socket.id) {
+            raiseAnswered(socket, msg.split(' ')[0]);
         }
-        else if(msg.split(' ')[0].charAt(0) !== currentChar && currentChar !== '' && userlist[idx].id === socket.id )
-        {
+        else if (msg.split(' ')[0].charAt(0) !== currentChar && currentChar !== '' && userlist[idx].id === socket.id) {
             raiseWrong(socket, msg.split(' ')[0]);
         }
     });
@@ -52,29 +58,51 @@ io.on('connection', function(socket) {
 
 function startGame() {
     idx = 0;
+    updateTurn();
+    io.emit('success', userlist[idx].username + '님이 10초 이내로 ' + currentChar + '로 시작하는 시작할 단어를 입력해주세요.');
     timeout = setTimeout(() => {
         raiseTimeout()
-    }, 7500);
-    io.emit('success', userlist[idx].username + '님이 7.5초 이내로 시작할 단어를 입력해주세요.');
+    }, 10000);
 }
 
 function raiseAnswered(socket, answer) {
+    if (blacklist.includes(answer)) {
+        raiseWrong(socket, answer);
+    }
     clearTimeout(timeout);
     io.emit('success', socket.username + '님의 단어 ' + answer + '가 인정되었습니다.');
     console.log(socket.username + '님의 단어 ' + answer + '가 인정되었습니다.');
     currentChar = answer.charAt(answer.length - 1);
-    if(++idx === userlist.length)
-    {
+    if (++idx === userlist.length) {
         idx = 0;
     }
-    io.emit('success', userlist[idx].username + '님이 7.5초 이내로 ' + currentChar + "로 시직하는 단어를 입력해주세요.");
+    updateTurn();
+    io.emit('success', userlist[idx].username + '님이 10초 이내로 ' + currentChar + "로 시직하는 단어를 입력해주세요.");
     timeout = setTimeout(() => {
         raiseTimeout()
-    }, 7500);
+    }, 10000);
+}
+
+function updateUsersList() {
+    io.emit('userListUpdate', userlist.map(function (i) {
+        return {
+            username: i.username,
+            color: i.color
+        }
+    }));
+}
+
+function updateTurn() {
+    io.emit('turn', idx);
+}
+
+function raiseWrong(socket, answer) {
+    io.emit('alert', socket.username + '님의 단어 ' + answer + '는 인정되지 않습니다.');
 }
 
 function raiseTimeout() { //시간초과
     io.emit('alert', '시간이 다 되었습니다. 게임이 종료됩니다.');
+    io.emit('gameEnd');
     currentChar = '';
 }
 
